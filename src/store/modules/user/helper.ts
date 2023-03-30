a95c415c17245c180e151fb9307b04243f99554c
Dooy <dooy520@qq.com>
2023-03-30 09:25:57 +0800

    viersion

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -16,8 +16,8 @@ export function defaultSetting(): UserState {
   return {
     userInfo: {
       avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      name: 'AiDuTu',
+      description: '友链 <a href="https://www.aidutu.cn" class="text-blue-500" target="_blank" >大麦商品</a>',
     },
   }
 }
