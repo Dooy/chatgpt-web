2abc9ede54d44d576dfefff2c043e0c641b22689
Dooy <dooy520@qq.com>
2023-05-22 20:20:05 +0800

    新的起点

--- src/views/aidutu/aiGongGao.vue
+++ src/views/aidutu/aiGongGao.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ajax } from '@/api';
 import { useUserStore } from '@/store';
+import { onMounted , ref} from 'vue'
 const userStore = useUserStore()
 
 function load(){
@@ -9,6 +10,7 @@ function load(){
         userStore.updateUserInfo(d.data.cf)
         document.title = d.data.cf.name
         baidu(  d.data.cf.tj);
+        st.value.mark= d.data.cf.mark;
     }
     );
 }
@@ -19,8 +21,39 @@ function baidu( id:string ){
 		var s = document.getElementsByTagName("script")[0];
 		s.parentNode.insertBefore(hm, s);
 }
+function createWatermark() {
+    
+     let i2=200 ,i3=230;
+     for ( let j=0;j<30;j++){
+        let top2=-100+(j*i3) ;
+        for(let i=0; i<30;i++){
+            let left= (-120+i*i2+ Math.random()*100)+'px';
+            let top = (top2+ Math.random()*100)+'px';
+            st.value.arr.push({left,top}) ;
+        }
+     }
+     
+
+}
+const st = ref({mark:'',arr:[]}); //UID:239923
+onMounted(()=>{
+    createWatermark();
+});
 load();
 </script>
 <template>
-
+<template v-if="st.mark">
+    <span v-html="st.mark" class="fls" :style="s" v-for="s in st.arr"></span>
 </template>
+</template>
+<style scoped>
+.fls{
+    font-size: 14px;
+color: rgba(0, 0, 0, 0.07);
+position: fixed;
+transform: rotate(-30deg);
+padding:100px;
+pointer-events: none;
+z-index: 200;
+}
+</style>
