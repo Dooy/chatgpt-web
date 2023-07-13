a455645be75d138459be1f6e001a315715444d3c
Dooy <dooy520@qq.com>
2023-07-13 12:43:01 +0800

    sse server

--- service/src/sse/rabittmq.ts
+++ service/src/sse/rabittmq.ts
@@ -9,6 +9,15 @@ async function createRabbitMQChannel() {
     return ch ;
 }
 
+async function createRabbitMQChannelReal() {
+    const r = createRabbitMQ();
+    const c = await r.acquire();
+    c.on('close', () => {
+        console.log('channel was closed')
+    });
+    return {r,c} ;
+}
+
 function createRabbitMQ(){
     const r= new Connection({
     url:   <string>process.env.SSE_MQ_SERVER, 
@@ -25,13 +34,28 @@ export async function publishData( exchange:string,routingKey:string, data:strin
     //await ch.queueDeclare({queue , exclusive: true}) 
     console.log('publishData>>',exchange,routingKey )
     try{
+        const mq = await createRabbitMQChannelReal();
+        await mq.c.basicPublish({routingKey,exchange}, data) 
+        await mq.c.close();
+        await mq.r.close();
+    }catch(e ){
+         //console.error('publishData 发生错误>>', e  )
+        await  publishData_bak(exchange,routingKey, data );
+    }
+}
+async function publishData_bak( exchange:string,routingKey:string, data:string) {
+   
+    //await ch.queueDeclare({queue , exclusive: true}) 
+    console.log('publishData_bak>>',exchange,routingKey )
+    try{
         await createRabbitMQChannel();
         await ch.basicPublish({routingKey,exchange}, data) 
         await closeMq();
     }catch(e ){
-         console.error('publishData 发生错误>>', e  )
+         console.error('publishData_bak 发生错误>>', e  )
     }
 }
+
 async function closeMq(){
     await ch.close();
     await rabbit.close();
