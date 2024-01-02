9bf06824e23b2533f98759917cd218cd5e0b7281
Dooy <dooy520@qq.com>
2024-01-02 09:48:42 +0800

    gpts

--- service/src/sse/gptscopilot.ts
+++ service/src/sse/gptscopilot.ts
@@ -11,10 +11,9 @@ import { writeAidutu } from "src/utils";
 
 
 const GPTS_COOKIE = process.env.GPTS_COOKIE
-
+let reqCount = 0;
 
 const fetchSSEQuery =  async  (request:Request, response:Response,messageBody:any,msg:msgType )=>{
-   
     mlog('baseurl', msg.isStream  );
     const url=`https://gptscopilot.ai/api/chat/completions` 
     const headers ={
@@ -32,18 +31,20 @@ const fetchSSEQuery =  async  (request:Request, response:Response,messageBody:an
     const oldModel= messageBody.model ;
     //gpt-4-gizmo-
     if( messageBody.model ) messageBody.model=  messageBody.model.replace(/gpt-4-gizmo-/ig,'');
-    if(messageBody.model=='gpt-4-all')messageBody.model=  process.env.GPTS_ALL? process.env.GPTS_ALL: "g-l23NycKzK";
-    mlog('log','model', oldModel , messageBody.model );
+    if(messageBody.model=='gpt-4-all')messageBody.model='gpt-4-all' ;// process.env.GPTS_ALL? process.env.GPTS_ALL: "g-l23NycKzK";
+    mlog('log','model', oldModel , messageBody.model, reqCount  );
 
     const body = JSON.stringify( messageBody );
     const id= 'chatcmpl-'+ generateRandomCode(30); 
     msg.id= id ;
-    let isGo=false;
+    let isGo= reqCount<=0 ? false :true ;
     let isDoing= false;  
     let isError=false;
  
     let oldData='';
+    let arrDataString: string[]= [];
     try { 
+       reqCount++;
        const res = await fetchSSE( url,{
             method: 'POST',
             headers: headers,
@@ -54,13 +55,14 @@ const fetchSSEQuery =  async  (request:Request, response:Response,messageBody:an
                  }
                  
                  if(!isGo) {
-                    let ddata=''
-                    try{
-                        let pjson= JSON.parse(data );
-                        ddata= pjson.choices[0].delta.content;
-                    }catch(e3){
-                    }
-                    if(
+                    // let ddata=''
+                    // try{
+                    //     let pjson= JSON.parse(data );
+                    //     ddata= pjson.choices[0].delta.content;
+                    // }catch(e3){
+                    // }
+                    let ddata= getStreamContent(data);
+                    if( 
                     data.indexOf('We have run out of conversations today. Please try again tomorrow.')>-1
                     || ddata.indexOf('今日对话次数已用完，请明日再试')>-1
                      ){
@@ -81,11 +83,15 @@ const fetchSSEQuery =  async  (request:Request, response:Response,messageBody:an
                  else if(data=='[DONE]'){
                     response.write( `data: ${data}\n` );  
                     response.write(  "\n"); 
-                 }else if(oldData ){
-                    response.write( `data: ${oldData}\n` );  
-                    response.write(  "\n"); 
+                 }else{ 
+                    if(oldData ){
+                        response.write( `data: ${oldData}\n` );  
+                        response.write(  "\n"); 
+                    }
+                    arrDataString.push( getStreamContent(data)); //每个chunk结果
                  }
                  oldData= data;
+               
             },
             onError(e ){
                 //console.log('eee>>', e )
@@ -98,38 +104,58 @@ const fetchSSEQuery =  async  (request:Request, response:Response,messageBody:an
      } catch (e ) {
        mlog('error','food',e)
         //response.send(2)
-        if(e.status) {
-            response.writeHead(e.status );
-            if(e.status==403){
-                writeAidutu({'error':'可能是因为cookie 失效因为账号不存在' ,status:403 });
+        try{
+            if(e.status) {
+                response.writeHead(e.status );
+                if(e.status==403){
+                    writeAidutu({'error':'可能是因为cookie 失效因为账号不存在' ,status:403 });
+                }
+                //publishData( "openapi", 'error',  JSON.stringify({e,tomq} ));
+                //response.end( e.reason?.replace(/one_api_error/ig,'openai_hk_error'));
+                //let ss = e.reason??'gate way error...';
+                let obj={error:{"message":e.statusText,  "type":"openai_hk_error","code":e.status}}
+                response.end(  JSON.stringify(obj)  );
+                return ;
+                
             }
-            //publishData( "openapi", 'error',  JSON.stringify({e,tomq} ));
-            //response.end( e.reason?.replace(/one_api_error/ig,'openai_hk_error'));
-            //let ss = e.reason??'gate way error...';
-            let obj={error:{"message":e.statusText,  "type":"openai_hk_error","code":e.status}}
-            response.end(  JSON.stringify(obj)  );
-            return ;
-            
-        }
-        else {
-            response.writeHead(428);
-            if( e.statusCode==403 ){
-                  writeAidutu({'error':'可能是因为cookie 失效因为账号不存在' ,status:403 });
-            }
-            //response.end("get way error...\n"  );
-            let ss = e.reason??'gate way error...';
-            response.end( `{"error":{"message":"${ss}","type":"openai_hk_error","code":"gate_way_error"}}`   );
-            mlog('error', ss ,e )
-            //publishData( "openapi", 'error',  JSON.stringify({e: {status:428,reason:e}, tomq }));
+            else if( e.statusCode ) {
+                response.writeHead(428);
+                if( e.statusCode==403 ){
+                    writeAidutu({'error':'可能是因为cookie 失效因为账号不存在' ,status:403 });
+                }
+                //response.end("get way error...\n"  );
+                let ss = e.reason??'gate way error...';
+                response.end( `{"error":{"message":"${ss}","type":"openai_hk_error","code":"gate_way_error"}}`   );
+                mlog('error', ss ,e )
+                //publishData( "openapi", 'error',  JSON.stringify({e: {status:428,reason:e}, tomq }));
+                return ;
+            } 
+        }catch(e2 ){
+            mlog('error','food-gate-way-error');
+            response.end( `{"error":{"message":"gate way error...","type":"openai_hk_error","code":"gate_way_error"}}`   );
             return ;
         }
     } 
-    
+    mlog('log','结果cnt：', arrDataString.length ,reqCount , arrDataString[0] );
+    if(msg.isStream &&  arrDataString.length<=1 && arrDataString[0]=='' && reqCount<=1 ){
+         mlog('log','重复中 repost' );
+         fetchSSEQuery(request, response,messageBody,msg  ) //request:Request, response:Response,messageBody:any,msg
+         return ;
+    }
     response.end();
 
 }
 
-
+const getStreamContent =( data:string)=>{
+    let ddata=''
+    try{
+        let pjson= JSON.parse(data );
+        ddata= pjson.choices[0].delta.content as string;
+        return ddata;
+    }catch(e3){
+        return '';
+    }
+}
 
 //主程序
 export const gptscopilot=  async  ( request:Request, response:Response, next?:NextFunction)=>{
