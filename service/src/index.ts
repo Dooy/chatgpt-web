ecf1c7ca86dd6fe88bf5e803a1f616b1e7ee966c
Dooy <dooy520@qq.com>
2024-03-20 08:47:53 +0800

    channel

--- service/src/index.ts
+++ service/src/index.ts
@@ -54,10 +54,12 @@ import multer from "multer"
 		} catch (error) {
 			try {
 				if (error.message && 
-				 (error.message.indexOf('check your plan and billing details') > 0
-				  || error.message.indexOf('Incorrect API key provided') > 0 
-				  || error.message.indexOf('billing details') > 0  //跟账单相关的都去掉
-				  || error.message.indexOf('deactivated account') > 0  //跟账单相关的都去掉
+				 (
+				//  error.message.indexOf('check your plan and billing details') > 0
+				//   || error.message.indexOf('Incorrect API key provided') > 0 
+				//   || error.message.indexOf('billing details') > 0  //跟账单相关的都去掉
+				//   || error.message.indexOf('deactivated account') > 0  //跟账单相关的都去掉
+					error.message.indexOf('无可用渠道')>0
 				) ) {
 					sharedData.cnt++;
 					sharedData.error_des = error;
@@ -103,10 +105,12 @@ import multer from "multer"
 	} catch (error) {
 		try {
 			if (error.message &&  
-			(error.message.indexOf('check your plan and billing details') > 0 
-			|| error.message.indexOf('Incorrect API key provided') > 0 
-			|| error.message.indexOf('billing details') > 0  //跟账单相关的都去掉
-			|| error.message.indexOf('deactivated account') > 0  //跟账单相关的都去掉
+			(
+			// error.message.indexOf('check your plan and billing details') > 0 
+			// || error.message.indexOf('Incorrect API key provided') > 0 
+			// || error.message.indexOf('billing details') > 0  //跟账单相关的都去掉
+			// || error.message.indexOf('deactivated account') > 0  //跟账单相关的都去掉
+			error.message.indexOf('无可用渠道')>0
 			) ) {
 				sharedData.cnt++;
 				sharedData.error_des = error;
