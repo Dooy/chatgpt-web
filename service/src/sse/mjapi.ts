a19e5d915e17b60060e63aa2b860cb6edd5ec25b
Dooy <dooy520@qq.com>
2024-03-25 15:32:35 +0800

    relax slow

--- service/src/sse/mjapi.ts
+++ service/src/sse/mjapi.ts
@@ -5,23 +5,49 @@ import { publishData } from './rabittmq';
 import { isNotEmptyString } from 'src/utils/is';
 import { fetchSSE } from './fetch-sse';
 
+export interface changType{
+	uri:string
+	mode:string
+}	
+
 const fast_uid = isNotEmptyString(process.env.FAST_UID)? process.env.FAST_UID.split(',').map(v=>v.trim() ) :[];
-const changBody = ( body:any ,idArr:number[],uri:string )=>{
-    //mlog('changBody', body);
+
+const changBody = ( body:any ,idArr:number[],cType:changType)=>{
+    //mlog('changBody', body);  
     let rz = body;
 	const uid= idArr[0];
+	const uri= cType.uri
     const notifyHook=`${process.env.SSE_HTTP_SERVER}/openai/mjapi/${uid}-${idArr[1]}` 
     rz.notifyHook= notifyHook +'/'+ ( rz.notifyHook?encodeURIComponent( rz.notifyHook ):'');
-    if(!rz.state || rz.state=='')rz.state= `${uid}` ;
-	mlog( 'Fast UID=',uid, fast_uid )
-	if(uri=='/mj/submit/imagine' && fast_uid.indexOf( `${uid}`)>-1 ){
-		mlog('log','Fast UID=',uid )
-		if(rz.accountFilter) rz.accountFilter.modes=['FAST']
-		else rz.accountFilter= {modes:['FAST'] }
+	const isRelax= cType.mode=='slow' || cType.mode=='relax'
+	if (isRelax && uri!='/mj/insight-face/swap'){
+		 rz.notifyHook= notifyHook +`-relax/`+ ( rz.notifyHook?encodeURIComponent( rz.notifyHook ):'');
 	}
+    if(!rz.state || rz.state=='')rz.state= `${uid}` ;
+	//mlog( 'Fast UID=',uid, fast_uid )
+	const subArr=['/mj/submit/imagine','/mj/submit/blend','/mj/submit/describe','/mj/submit/action','/mj/submit/shorten' ];
+	if(  subArr.indexOf(uri)>-1 ){
+		
+		if (fast_uid.indexOf( `${uid}`)>-1 ||  cType.mode=='fast'  ) { //
+			mlog('log','Fast UID=',uid )
+			if(rz.accountFilter) rz.accountFilter.modes=['FAST']
+			else rz.accountFilter= {modes:['FAST'] }
+		}
+		if(  isRelax  ){
+			mlog('log','慢速 ',uid )
+			if(rz.accountFilter) rz.accountFilter.modes=['RELAX'] //relax
+			else rz.accountFilter= {modes:['RELAX'] }
+		}
+
+	}
+	
+	
     mlog('changBody>> ',   rz.notifyHook );
+
     return rz ;
 }
+
+
 
 export const  mjapi = async  ( request:Request, response:Response, next?:NextFunction)=> {
     const headers = {
@@ -41,7 +67,16 @@ export const  mjapi = async  ( request:Request, response:Response, next?:NextFun
     const url= isNotEmptyString( process.env.MJ_SERVER_URL)? process.env.MJ_SERVER_URL: 'http://43.154.119.189:6090';
     const userPsw=  isNotEmptyString( process.env.MJ_SERVER_USERPSW)? process.env.MJ_SERVER_USERPSW: "aitutu:20221116";
 
-	const uri= (request.headers['x-uri']??'/mj/submit/imagine') as string
+	let  uri= (request.headers['x-uri']??'/mj/submit/imagine') as string
+	let  xmode= (request.headers['x-mode']??'fast') as string
+	if (uri.indexOf('/fast/')==0){
+		xmode='fast'
+		uri=uri.substring( 5)
+	}
+	if(uri.indexOf('/relax/')==0){
+		xmode='relax'
+		uri=uri.substring(6)
+	}
     try{
             
             //const mykey=await getMyKey( request.headers['mj-api-secret']?? request.headers['authorization'], request.body);
@@ -67,7 +102,7 @@ export const  mjapi = async  ( request:Request, response:Response, next?:NextFun
 				}
 			}
 
-            const body=  JSON.stringify(  changBody(request.body,[ +mykey.user?.uid , son_id],uri ));
+            const body=  JSON.stringify(  changBody(request.body,[ +mykey.user?.uid , son_id],{uri,mode:xmode} ));
             //mlog( 'body' ,body )
 		    await fetchSSE( rqUrl ,{
                 method: 'POST',
